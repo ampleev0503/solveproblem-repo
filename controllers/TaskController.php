81f1e2d46c69b5f786240e03aac72256c04c4610
developer <ampleevv@dynamic-2a02-2698-5c24-0.penza.ertelecom.ru>
2018-05-20 05:43:21 +0400

    добавление массива категорий и соответсвующих подкатегорий

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -24,6 +24,22 @@ class TaskController extends Controller
         $itemsCategory = (new CategoryRepository())->getAll(); // получение всех категорий из бд
         $itemsSubcategory = (new SubcategoryRepository())->getAll(); // получение всех подкатегорий из бд
 
+        /** @var \app\models\Category[] $itemsCategory */
+        /** @var \app\models\Subcategory[] $itemsSubcategory */
+        foreach ($itemsCategory as $category)
+        {
+            foreach ($itemsSubcategory as $subcategory)
+            {
+                if ($category->id == $subcategory->categoryId)
+                {
+                    $dataCategory[$category->name][] = $subcategory->name;
+                }
+            }
+        }
+
+        //echo("<pre>");
+        //var_dump($dataCategory);
+
         echo $this->render("task/index", ['data' => $dataCategory]);
     }
 
